104b5314bb708d7e9cc8f22c0e7165a3a5bbd44c
Bob D'Errico <bob@bobderrico.com>
2015-03-14 20:48:21 -0400

    Sets up gulp tasks

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp');
+var minifyCSS = require('gulp-minify-css');
+var rename = require('gulp-rename');
+var sass = require('gulp-sass');
+
+var paths = {
+  sass: ['./scss/**/*.scss']
+};
+
+gulp.task('default', ['sass']);
+
+gulp.task('sass', function () {
+  return gulp.src('./scss/main.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./css/'))
+    .pipe(minifyCSS())
+    .pipe(rename({ extname : '.min.css'} ))
+    .pipe(gulp.dest('./css/'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch(paths.sass, ['sass']);
+});
