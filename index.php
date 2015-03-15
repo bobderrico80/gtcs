b2c4a14ac0dc8a2ef59ee2b819df88d81169a37b
Bob D'Errico <bob@bobderrico.com>
2015-03-14 23:30:44 -0400

    Completes header and navbar

--- index.php
+++ index.php
@@ -2,8 +2,43 @@
 <html>
   <head>
     <title>Green Team Cleanout Services</title>
+    <link href="./css/normalize.min.css" rel="stylesheet"/>
+    <link href="./css/main.min.css" rel="stylesheet"/> 
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,400,700' rel='stylesheet' type='text/css'>
+    <meta charset="utf8"/>
+    <meta name="viewport" content="width=device-width"/>
   </head>
   <body>
-    <h2 style="font-family: sans-serif">Welcome to the future home of Green Team Cleanout Services</h2>
+    <header>
+      <img class = "logo" src="./img/logo.png"/>
+      <div class="header-info">
+        <h1 class="title">Green Team Cleanout Services</h1>
+        <h2 class="tagline">Big or Small, We Do It All!</h2>
+        <h2 class="phone-number">856-282-5363</h2>
+      </div>
+    </header>
+    <nav> 
+      <ul>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">Services</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
+      </ul>
+    </nav>
+    <main>
+      <h1>Test Content</h1>
+    </main>
+    <footer>
+      <p>Copyright &copy 2015 Green Team Cleanout Services</p>
+      <p>Built by <a href="http://www.bobderrico.com">Bob D'Errico</a></p>
+    </footer>
   </body>
 </html>
